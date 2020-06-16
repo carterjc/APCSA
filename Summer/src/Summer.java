e6164ff60054949197c57251e93cb458169ed9f3
Carter Costic <cartercostic@gmail.com>
2020-06-15 20:00:51 -0400

    Summer work

---
+++ Summer/src/Summer.java
@@ -0,0 +1,46 @@
+// Carter Costic
+
+public class Summer {
+    public static void main(String[] args) {
+
+        // Hello World code
+
+        System.out.println("Hello, World!");
+
+        // Print name code
+
+        System.out.println("Carter Costic");
+
+        // myName var
+
+        String myName = "Carter Costic";
+
+        // Print myName
+
+        System.out.println(myName);
+
+        // Write code that will store your favorite number as a variable ‘favNum’ as an integer
+
+        int favNum = 19;
+
+        // Write code that will print the favNum variable
+
+        System.out.println(favNum);
+
+        // Write code that will print the sum of favNum and 5
+
+        System.out.println(favNum + 5);
+
+        // Create a variable of the type boolean. Provide it with a name and
+        // value. Print the variable
+
+        boolean x = true;
+        System.out.println(x);
+
+        // Create a variable of the type double. Provide it with a name and
+        // value. Print the variable
+
+        double y = 17.74;
+        System.out.println(y);
+    }
+}
